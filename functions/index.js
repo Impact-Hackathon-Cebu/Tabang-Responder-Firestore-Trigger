8a4e4118c8d30ce634866a40808ddac6ab579931
Cleo Credo <credocleo28@gmail.com>
2019-10-31 11:38:08 +0800

    initial files

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,8 @@
 'use strict';
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-
 const db = admin.firestore();
-const login = require("facebook-chat-api");
-
-// exports.receiveAlerts = functions.firestore
-//     .document('reports/{report}')
-//     .onCreate(async (snap, context) => {
-//         console.log("receive alerts")
-//         console.log("context: ", context)
-//         console.log("snap: ", snap)
-//         console.log("_fieldsProto: ", snap._fieldsProto)
-//         console.log("lat: ", snap._fieldsProto.lat)
-//         // console.log("lng: ", snap._fieldsProto.lng)
-//         // console.log("lng: ", snap._fieldsProto.emergency)
-
-//         // console.log('before data: ', snap.before.data())
-//         // console.log('after data: ', snap.after.data())
-        
-//         return snap
-//     });
+admin.initializeApp(functions.config().firebase);
 
     exports.receiveReports = functions.https.onRequest(async (req, res) => {
         console.log("req object: ", req)
@@ -40,9 +21,10 @@ const login = require("facebook-chat-api");
         .then(response => {
             console.log("response: ", response)
             const document_ref = response.path
+            const document_id = response.id
             console.log("document ref: ", response.path)
-            db.collection('/hospitals/Ffv7QjIh6VbiLiYTl8An/reports')
-            .add({
+            db.collection('/hospitals/Ffv7QjIh6VbiLiYTl8An/reports').doc(document_id)
+            .set({
                 reportReference: document_ref,
                 desc: req.body.text,
                 lng: 123.8888898,
@@ -54,12 +36,6 @@ const login = require("facebook-chat-api");
     })
 
     exports.receiveReports2 = functions.https.onRequest(async (req, res) => {
-        console.log("req object: ", req)
-        console.log("req METHOD: ", req.method)
-        console.log("data: ", req.params)
-        console.log("req: ", req.body)
-
-        
         db.collection('reports')
         .add({
             desc: req.body.text,
@@ -68,11 +44,10 @@ const login = require("facebook-chat-api");
             time: req.body.time
         })
         .then(response => {
-            console.log("response: ", response)
+            const document_id = response.id
             const document_ref = response.path
-            console.log("document ref: ", response.path)
-            db.collection('/baranggay/R85cO2Pf4St48nzYS2LW/reports')
-            .add({
+            db.collection('/baranggay/R85cO2Pf4St48nzYS2LW/reports').doc(document_id)
+            .set({
                 reportReference: document_ref,
                 desc: req.body.text,
                 lng: 123.8888898,
@@ -80,16 +55,6 @@ const login = require("facebook-chat-api");
                 time: req.body.time
             })
             res.send(response.path)
-        });
-    })
-
-    exports.fbMessaging = functions.https.onRequest(async (req, res) => {
-        login({email: "clarisselallaineerika", password: "P@ssword01"}, (err, api) => {
-            if(err) return console.error(err);
-        
-            var yourID = "000000000000000";
-            var msg = "Hey!";
-            api.sendMessage(msg, yourID);
         });
     })
     
